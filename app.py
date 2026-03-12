e6bd835507f60fc8de6ff120f3850d4694235d10
Diego Moysen <dieguito2019.lopez@gmail.com>
2026-03-11 20:30:37 -0600

    Fix dynamic port for Render

--- app.py
+++ app.py
@@ -1,5 +1,6 @@
 from flask import Flask, request, jsonify
 import pickle
+import os
 
 app = Flask(__name__)
 
@@ -21,4 +22,5 @@ def predict():
     })
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=10000)
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
